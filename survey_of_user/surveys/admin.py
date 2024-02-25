3fc07915245dabd196137bf9fd70646294cc459b
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-02-25 20:44:13 +0300

    Create models Survey & Question & Answer and all in admin.py

--- survey_of_user/surveys/admin.py
+++ survey_of_user/surveys/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Survey, Question, Answer
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "description", "slug")
+    list_display_links = ("title",)
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "text")
+    list_display_links = ("title",)
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "text")
+    list_display_links = ("title",)
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
